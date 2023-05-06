c380faa375047acad117b828598773bb2184004c
Challelign Tilahun <chalie2123@gmail.com>
2023-05-06 12:36:56 +0300

    Handling CastError While undifind value send to the server

--- backend/controllers/errorController.js
+++ backend/controllers/errorController.js
@@ -1,8 +1,16 @@
+const AppError = require('./../utils/appError');
+
+const handleValidationErrorDB = (err)=>{
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400)
+}
+
 module.exports = (err , req, res, next) =>{
 
     console.log(err.stack)
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error'
+
     res.status(err.statusCode).json({
         status:err.status,
         message:err.message
@@ -50,10 +58,11 @@ const sendErrorDev = (err, res) => {
       sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === 'production') {
       let error = { ...err };
-  
-      
+
+      if (error.name === "CastError") {
         error = handleValidationErrorDB(error);
-  
+      }
+
       sendErrorProd(error, res);
     }
   };
