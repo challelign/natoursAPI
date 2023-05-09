df344b7d57a49e3d6f5885a6132d44a3906a5533
Challelign Tilahun <chalie2123@gmail.com>
2023-05-09 14:48:12 +0300

    the recommended way for sending data

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,8 +1,19 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
-exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+exports.signup = catchAsync(async (req, res, next) => { 
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  // this also the same but if someone send req.body it will send extra filed different from the field below
+  // use the above method always , b/c allowd filed are sent only the listed one
+  // const {name ,email, password ,passwordConfirm } = req.body
+  // const newUser = await User.create(req.body);
+
 
   res.status(201).json({
     status: 'success',
